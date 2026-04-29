1f75523448013e460554a7bf473285ade492c691
Marc Robertson <robertsonmarc@myocu.oak.edu>
2026-04-28 20:21:04 -0500

    Revert "Updated MVP"
    This reverts commit 2ec9de5cebe00d48a782db73f99322198bc061f6.

--- back-end/server.js
+++ back-end/server.js
@@ -8,10 +8,10 @@ app.use(express.json());
 
 // 🔥 Create MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "tickets_db"
 });
 
 // 🔥 Connect to MySQL
